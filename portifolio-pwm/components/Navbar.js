99df3ae1ff563bfb0091553602886e186f224935
Nunno Wakiyama Diniz Carvalho <142551010+nunnowdc@users.noreply.github.com>
2025-09-26 16:35:36 +0000

    ajustes finos

--- portifolio-pwm/components/Navbar.js
+++ portifolio-pwm/components/Navbar.js
@@ -6,10 +6,10 @@ export default function Navbar() {
     <nav className={styles.navbar}>
       <div className={styles.brand}>Nunno W. D. Carvalho</div>
       <ul className={styles.navLinks}>
-        <li><Link href="#educacao">Educação</Link></li>
-        <li><Link href="#tecnologias">Tecnologias</Link></li>
-        <li><Link href="#projetos">Projetos</Link></li>
-        <li><Link href="/forca">Jogo da Forca</Link></li>
+        <li><Link href="#educacao" className={styles.navButton}>Educação</Link></li>
+        <li><Link href="#tecnologias" className={styles.navButton}>Tecnologias</Link></li>
+        <li><Link href="#projetos" className={styles.navButton}>Projetos</Link></li>
+        <li><Link href="/forca" className={styles.navButton}>Jogo da Forca</Link></li>
       </ul>
     </nav>
   );
